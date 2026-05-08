d0178897541827cd4d16058dbcf147e4ca763899
youngpave <dabdulbari8@gmail.com>
2026-05-09 00:50:08 +0300

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,8 +1,7 @@
 <?php
-// Yayıncı sitenin asıl kaynak adresi
 $url = "https://dlhd.pk/stream/stream-62.php";
 
-// Android kimliği (User-Agent)
+// Senin Xiaomi telefonunun kimliği
 $ua = "Mozilla/5.0 (Linux; Android 14; 23117RK66C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36";
 
 $ch = curl_init();
@@ -10,13 +9,14 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_USERAGENT, $ua);
-
-// Burası kritik: Yayıncı site senin oradan geldiğini sanmalı
 curl_setopt($ch, CURLOPT_REFERER, "https://dlhd.pk/");
 
-$response = curl_exec($ch);
+$html = curl_exec($ch);
 curl_close($ch);
 
-// İçeriği ekrana bas
-echo $response;
+// Sayfa içindeki linklerin bozulmaması için ana adresi ekliyoruz
+$base_url = '<base href="https://dlhd.pk/stream/">';
+$html = str_replace('<head>', '<head>' . $base_url, $html);
+
+echo $html;
 ?>
